dd2d7508f86bb6be47da36264594dce12d5d921a
Chandan Kumar <chandan@kaushalaminc.com>
2022-03-02 10:15:25 +0530

    added carausal changes and image below carusal

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,14 +29,8 @@ const home = () => {
         <Slide>
           {slideImages.map((slideImage, index) => (
             <div className="each-slide" key={index}>
-              <div
-                style={{
-                  backgroundImage: `url(${slideImage.url})`,
-                  height: "500px",
-
-                  backgroundRepeat: "no repeat",
-                }}
-              >
+              <div>
+                <img src={slideImage.url} className="crausalImage" />
                 <span>{slideImage.caption}</span>
               </div>
             </div>
@@ -48,12 +42,8 @@ const home = () => {
         <div class="w3-col m6 w3-padding-large w3-hide-small">
           <img
             src={image6}
-            class="w3-round w3-image w3-opacity-min"
+            class="w3-round w3-image w3-opacity-min ImgbelowCrausal"
             alt="Table Setting"
-            //width="800"
-            height="400"
-            // display="flex"
-            // float="left"
             justify-content="space-around"
             align-items="left"
           />
@@ -64,21 +54,18 @@ const home = () => {
           <br></br>
           <h5 class="w3-center">Tradi-tion since 1889</h5>
           <p class="w3-large">
-            The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor
-            sit amet, consectetur adipiscing elit consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.We only use{" "}
-            <span class="w3-tag w3-light-grey">seasonal</span> ingredients.
+            The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet,
+            consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.We only use <span class="w3-tag w3-light-grey">seasonal</span> ingredients.
           </p>
           <p class="w3-large w3-text-grey w3-hide-medium">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum consectetur adipiscing
-            elit, sed do eiusmod temporincididunt ut labore et dolore magna s
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt
+            ut labore et dolore magna s aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
       </div>
